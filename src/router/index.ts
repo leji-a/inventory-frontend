e14c74e51d1f20800f061b24168f71f0336ff044
leji-a <toviya3135@gmail.com>
2025-11-17 10:04:19 -0300

    basic router file

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+import LoginView from '../views/LoginView.vue'
+import SignupView from '../views/SignUpView.vue'
+import DashboardView from '../views/DashboardView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: DashboardView, meta: { requiresAuth: true } },
+    { path: '/login', component: LoginView },
+    { path: '/signup', component: SignupView },
+  ],
+})
+
+router.beforeEach(async (to, _from, next) => {
+  const auth = useAuthStore()
+  await auth.rehydrate()
+
+  if (to.meta.requiresAuth && !auth.token) next('/login')
+  else if ((to.path === '/login' || to.path === '/signup') && auth.token)
+    next('/')
+  else next()
+})
+
+export default router
